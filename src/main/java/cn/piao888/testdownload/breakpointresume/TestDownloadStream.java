1e1cadece57ca27379dd8dd9260bdec51fde3799
许鸿志 <847118663@qq.com>
2021-11-02 08:50:20 +0800

    断点续传真厉害

--- src/main/java/cn/piao888/testdownload/breakpointresume/TestDownloadStream.java
+++ src/main/java/cn/piao888/testdownload/breakpointresume/TestDownloadStream.java
@@ -26,18 +26,20 @@ public class TestDownloadStream {
 //                RandomAccessFile input = new RandomAccessFile("D:\\迅雷下载\\zh-cn_windows_10.iso", "R");
                 RandomAccessFile out = new RandomAccessFile("C:\\Users\\DELL\\Desktop\\zypt.sql", "rw")) {
             jedis.set("size", input.length() + "");
+            out.setLength(input.length());
             while (true) {
                 //确定指针读取到哪里了,如果是第一次读，肯定 从redis中获取到的是一个空的
+                //这个地方控制开始写入的起始位置
                 input.seek(Long.valueOf(jedis.get("position") == null ? "0" : jedis.get("position")));
                 out.seek(Long.valueOf(jedis.get("position") == null ? "0" : jedis.get("position")));
                 //读取字节到buff中,i可以真实的描述字节读取了多少
                 i = input.read(buff);
-                if(i==-1)break;
+                if (i == -1) break;
                 //一定要填i 写入长度，否则有可能回 将 bytes多余的字节信息 写到文件里面
                 out.write(buff, 0, i);
                 //当读取完与写入完毕后，偏移指针
                 jedis.set("position", jedis.get("position") == null ? i + "" : Long.valueOf(jedis.get("position")) + i + "");
-//                break;
+                break;
             }
             if (i == -1) {
                 jedis.del("position", "size");
